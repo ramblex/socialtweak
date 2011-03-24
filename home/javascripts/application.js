576ba5daa8cf83a8f7b5859c89b8c4b85c882ba9
Alex Duller <alexduller@gmail.com>
2011-03-24 18:55:42 +0000

    Added history to ajax pages

---
+++ home/javascripts/application.js
@@ -0,0 +1,51 @@
+// Assumes that jquery and jquery.history have been loaded
+
+$(document).ready(function() {
+
+  // Javascript that affects #content
+  function init_content(page) {
+    $("#slidedeck dl").slidedeck();
+
+    // Highlight the correct link
+    if (page != '') {
+      $('#nav a').removeClass('selected');
+      $('#nav a[href="'+page+'"]').addClass('selected');
+    }
+  }
+
+  $.history.init(function(page) {
+    $('#content').html('<div id="loading">Loading...</div>');
+    $('#content').load(page + ' #content', function() {
+      init_content(page);
+    });
+  });
+
+  $("#nav a").click(function() {
+    var url = $(this).attr('href');
+    url = url.replace(/^.*#/, '');
+    $.history.load(url);
+    return false;
+  });
+
+  $("#open-footer").hide();
+  $("#settings").click(function() {
+    $("#open-footer").slideToggle('fast', function() {
+      // Switch the footer arrow
+      if ($(this).is(':hidden')) {
+        $("#settings").css('background-image', 'url(images/footer-arrow.png)');
+      } else {
+        $("#settings").css('background-image', 'url(images/footer-arrow-down.png)');
+      }
+    });
+  });
+
+  // Background chooser
+  $("#choose-background a").click(function() {
+    var name = $(this).attr('name');
+    var options = "";
+    if (name == "white") {
+      options += " no-repeat";
+    }
+    $('body').css('background-image', 'url(images/backgrounds/'+$(this).attr('name')+'.png)' + options);
+  });
+});
